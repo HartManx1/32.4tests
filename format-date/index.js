234b0783153377545de95284de567f6681396711
Martin Hartman <hartmanx1@wp.pl>
2019-09-10 02:03:30 +0200

    32.4tests done

---
+++ format-date/index.js
@@ -0,0 +1,21 @@
+const formatDate = (timeInSeconds) => {
+
+    let hour = timeInSeconds / 3600;
+    let rest = timeInSeconds % 3600;
+    let min = rest / 60;
+    let sec = rest % 60;
+    let time = '';
+
+
+    if (Math.floor(hour) > 0) { time += Math.floor(hour) + 'h '; }
+
+    if (Math.floor(min) > 0) { time += Math.floor(min) + 'm '; }
+
+    if (Math.floor(sec) > 0) { time += Math.floor(sec) + 's'; }
+
+    if (!time.length)
+        time = '0s';
+    return time.trim();
+}
+
+module.exports = formatDate;
